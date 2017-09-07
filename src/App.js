00f429922a944fd4693e922fa0edfb5dbc44e5cd
Prijesh Meppayil <prijesh1987@gmail.com>
2017-09-07 11:28:44 +0530

    ContactList with hardcoded JSON array

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+class ContactList extends Component {
+  render() {
+    const people = [
+      {name: 'Prijesh'},
+      {name: 'Jijina'},
+      {name: 'Aadhith'}
+    ]
+
+    return <ol>{people.map(person => (<li key={person.name}>{person.name}</li>))}</ol>
+  }
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ContactList/>
+        <ContactList/>
       </div>
     );
   }
