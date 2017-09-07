7479c1691f3c0c2bcce5e324ffecc568366ffa90
Prijesh Meppayil <prijesh1987@gmail.com>
2017-09-07 14:54:06 +0530

    Passing contacts to ListContacts component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts';
 
-class ContactList extends Component {
-  render() {
-    const people = this.props.contacts;
-
-    return <ol>{people.map(person => (<li key={person.name}>{person.name}</li>))}</ol>
+const contacts = [
+  {
+    "id": "ryan",
+    "name": "Ryan Florence",
+    "email": "ryan@reacttraining.com",
+    "avatarURL": "http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id": "michael",
+    "name": "Michael Jackson",
+    "email": "michael@reacttraining.com",
+    "avatarURL": "http://localhost:5001/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "email": "tyler@reacttraining.com",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
   }
-}
+];
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ContactList contacts = {[
-          {name: 'Prijesh'},
-          {name: 'Jijina'},
-          {name: 'Aadhith'}
-        ]}/>
-        <ContactList contacts = {[
-          {name: 'Insaf'},
-          {name: 'Sijo'},
-          {name: 'Albert'},
-          {name: 'Sachin'}
-        ]}/>
+      <div>
+        <ListContacts contacts={contacts}/>
       </div>
     );
   }
